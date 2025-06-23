79bd31447a2eecbfa04a262c7e943bbaacc9abba
Bharath Katabattula <bharath632@revature.com>
2025-06-23 17:27:43 +0530

    URL issue fix

--- remote_agent_connection.py
+++ remote_agent_connection.py
@@ -20,10 +20,12 @@ class RemoteAgentConnections:
     """A class to hold the connections to the remote agents."""
 
     def __init__(self, agent_card: AgentCard, agent_url: str):
+        self._url = "https://disaster-management-871861759609.us-east4.run.app"
         print(f"agent_card: {agent_card}")
         print(f"agent_url: {agent_url}")
+        print(f"_URL : {self._url}")
         self._httpx_client = httpx.AsyncClient(timeout=30)
-        self.agent_client = A2AClient(self._httpx_client, agent_card, url=agent_url)
+        self.agent_client = A2AClient(self._httpx_client, agent_card, url=self._url)
         self.card = agent_card
         self.conversation_name = None
         self.conversation = None
